7932ba6b7285dffbc0d0ede8f29c5921d77e7dce
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2024-08-29 14:22:10 +0900

    Initial Commit

---
+++ birdman-todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  birdman-todo
+//
+//  Created by user on 2024/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ birdman-todo/birdman_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  birdman_todoApp.swift
+//  birdman-todo
+//
+//  Created by user on 2024/08/29.
+//
+
+import SwiftUI
+
+@main
+struct birdman_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
